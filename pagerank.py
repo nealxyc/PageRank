e797d0201be3a6b741bd4949650368da697eb800
Neal Xiong <nealxyc@gmail.com>
2015-03-04 00:15:20 -0800

    transform Graph and remove inner [] comprehesion

--- pagerank.py
+++ pagerank.py
@@ -23,19 +23,33 @@ def readGraph(lines):
 def diffVector(l1, l2,rang):
   return sum([abs(l1[i] - l2[i]) for i in rang])
 
+def transformGraph(nodes, inG, g):
+  idxInG = {} # instead of storing node number, store index of node -> index of in-nodexs
+  idxG = {}
+  idx = dict(zip(nodes, range(len(nodes))))
+  for node, inNodes in inG.iteritems():
+    idxInG[idx[node]] = set([idx[inNode] for inNode in inNodes])
+  for node, outdegree in g.iteritems():
+    idxG[idx[node]] = outdegree
+  return idxInG, idxG
+
 def pageRank(nodes, g, inG, beta=0.8, e=1e-8, n=1e3):
   r = array('f',[1.0/len(nodes)] * len(nodes))
   r_ = array('f', r)
   rang = array('l', range(len(r)))
   diff = e + 1
   t = 1
-  idx = dict(zip(nodes, rang)) # maps node number -> array index
   while diff > e and (n == -1 or t < n):
     sum_r = 0.0
-    for _i in rang:
-      j = nodes[_i]
-      r_[_i] = beta * sum([r[idx[i]]/g[i] for i in inG[j]]) if j in inG else 0.0
-      sum_r += r_[_i]
+    for j in rang:
+      rj = 0.0
+      if j in inG:
+	for i in inG[j]:
+	  rj += r[i]/g[i]
+	r_[j] = beta * rj
+	sum_r += r_[j] 
+      else:
+	r_[j] = 0.0
     leaked = 1 - sum_r
     if leaked > 0:
       incre = leaked/len(r_)
@@ -46,17 +60,6 @@ def pageRank(nodes, g, inG, beta=0.8, e=1e-8, n=1e3):
     t += 1
   return r, t
     
-def iterate(r, r_, beta, nodes,  g, inG, idx):
-  for _i, j in enumerate(nodes):
-    r_[_i] = beta * sum([r[idx[i]]/g[i] for i in inG[j]]) if j in inG else 0.0
-#  r_ = [beta*sum([r[idx[i]]/g[i]) for i in inG[j]]) if j in inG else 0.0 for j in nodes]
-  leaked = 1 - sum(r_)
-  if leaked > 0:
-    incre = leaked/len(r_)
-    for _i in range(len(r_)):
-      r_[_i] += incre
-  return r_
-
 if __name__ == '__main__':
   import fileinput
   import sys
@@ -65,12 +68,17 @@ if __name__ == '__main__':
   timer = time.time()
   lines = fileinput.input()
   g, inG, nodes = readGraph(lines)
-  print >> sys.stderr, 'Read graph: {0} seconds.'.format(time.time() - timer)
-  print >> sys.stderr, 'Read {0} nodes in graph'.format(len(nodes))
-  timer = 0 - time.time() 
-  r, t = pageRank(nodes, g, inG)
+  print >> sys.stderr, 'Read graph: {0:.1f} seconds.'.format(time.time() - timer)
+
+  timer = 0 - time.time()
+  idxInG, idxG = transformGraph(nodes, inG, g)
   timer += time.time()
-  print >> sys.stderr, 'Page Rank: {0} seconds.'.format(timer)
+  print >> sys.stderr, 'Preprocess graph: {0:.1f} seconds'.format(timer)
+
+  timer = 0 - time.time() 
+  r, t = pageRank(nodes, idxG, idxInG)
+  timer += time.time()
+  print >> sys.stderr, 'Page Rank: {0:.1f} seconds.'.format(timer)
   for idx, i in enumerate(r):
     print nodes[idx], i
   print >> sys.stderr, 'Done after {0} iterations.'.format(t)
