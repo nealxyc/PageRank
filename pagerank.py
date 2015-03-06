77251b4596e0ba3bf5dcf97813a22bae717cdf5b
Neal Xiong <nealxyc@gmail.com>
2015-03-06 00:54:54 -0800

    Moved out-graph into a long 1D array

--- pagerank.py
+++ pagerank.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 from array import array
+from ctypes import c_double,pointer
 
 def readGraph(lines):
   g = {} # node number -> out-link counts 
@@ -19,7 +20,7 @@ def readGraph(lines):
     nodes.add(v)
   return g, inG, sorted(nodes)
 
-def diffVector(l1, l2):
+def diff_vector(l1, l2):
   s = 0.0
   i = 0
   size = len(l1)
@@ -38,6 +39,19 @@ def transformGraph(nodes, inG, g):
     idxG[idx[node]] = set([idx[outNode] for outNode in outNodes])
   return idxInG, idxG
 
+def transformToArray(nodes, inG, g):
+  a = array('l')
+  idxInG, idxG = transformGraph(nodes, inG, g)
+  for i in range(len(nodes)): #[ <size>, <out node list>]
+    if i not in idxG:
+      a.append(0)
+      continue
+    outNodes = idxG[i]
+    a.append(len(outNodes))
+    a.extend(outNodes)
+  return idxInG, a
+
+
 def fillVector(r, num):
   i = 0
   size = len(r)
@@ -52,36 +66,49 @@ def forEachElement(r, func):
     r[i] = func(r[i], i)
     i += 1
 
-def pageRank(nodes, g, inG, beta=0.8, e=1e-8, n=1e2, debug=False):
+def pageRank(nodes, g, inG, beta=0.8, e=1e-8, n=1e2):
   
   r = array('d',[1.0/len(nodes)] * len(nodes))
   r_ = array('d', r)
 
   size = len(nodes)
+  gSize = len(g)
   diff = e + 1
   t = 1
   while diff > e and (n == -1 or t < n):
     sum_r = 0.0
-    i = 0
     # fill in 0s
     fillVector(r_, 0.0)
-    for i,jSet in g.iteritems():
-      scoreShare = r[i]/len(jSet)
-      sum_r += r[i]
-      for j in jSet:
-        r_[j] += scoreShare
+
+    # start iteration
+    i = 0
+    row_start = 0
+    while i < size:
+      row_size = g[row_start]
+      row_end = row_start + 1
+      if row_size:
+        score_share = r[i] / row_size 
+        sum_r += r[i]
+        cursor = row_end # same as row_start + 1
+	row_end = cursor + row_size
+        while cursor < row_end:
+	  r_[ g[cursor] ] += score_share
+	  cursor += 1
+      row_start = row_end
+      i += 1
+    
     # multiply by beta
     sum_r = sum_r * beta
     leaked = 1 - sum_r
-    leakedShare = leaked / size if leaked > 0 else 0.0
+    leaked_share = leaked / size if leaked > 0 else 0.0
     
     _i = 0
     while _i < size:
       # multiply by beta and fix leak at the same time
-      r_[_i] = r_[_i] * beta + leakedShare
+      r_[_i] = r_[_i] * beta + leaked_share
       _i +=1
     
-    diff = diffVector(r, r_)
+    diff = diff_vector(r, r_)
     r, r_ = r_, r
     t += 1
   return r, t
@@ -90,19 +117,21 @@ if __name__ == '__main__':
   import fileinput
   import sys
   import time
+  import os
   
+  print >> sys.stderr, 'PID {0}'.format(os.getpid())
   timer = time.time()
   lines = fileinput.input()
   g, inG, nodes = readGraph(lines)
   print >> sys.stderr, 'Read graph: {0:.1f} seconds.'.format(time.time() - timer)
 
   timer = 0 - time.time()
-  inG, g = transformGraph(nodes, inG, g)
+  inG, g = transformToArray(nodes, inG, g)
   timer += time.time()
   print >> sys.stderr, 'Preprocess graph: {0:.1f} seconds'.format(timer)
 
   timer = 0 - time.time() 
-  r, t = pageRank(nodes, g, inG, debug=True)
+  r, t = pageRank(nodes, g, inG)
   timer += time.time()
   print >> sys.stderr, 'Page Rank: {0:.1f} seconds.'.format(timer)
   for idx, i in enumerate(r):
