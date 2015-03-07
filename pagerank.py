5ff48ab24582e956e5b59520059d7ee45139aaf4
Neal Xiong <nealxyc@gmail.com>
2015-03-07 00:08:04 -0800

    flat the graph into 1D array

--- pagerank.py
+++ pagerank.py
@@ -8,7 +8,7 @@ from ctypes import c_double
 
 def readGraph(lines):
   g = {} # node number -> out-link counts 
-  inG = {} # node number -> set of in-link node numbers
+  ing = {} # node number -> set of in-link node numbers
   nodes = set() # all nodes
   for line in lines:
     #line = line.strip()
@@ -20,10 +20,10 @@ def readGraph(lines):
     #g.setdefault(k, set()).add(v)
     g.setdefault(k, 0)
     g[k] += 1
-    inG.setdefault(v, set()).add(k)
+    ing.setdefault(v, set()).add(k)
     nodes.add(k)
     nodes.add(v)
-  return g, inG, sorted(nodes)
+  return g, ing, sorted(nodes)
 
 def diffVector(l1, l2, lower=0, higher=0):
   s = 0.0
@@ -34,38 +34,40 @@ def diffVector(l1, l2, lower=0, higher=0):
     i += 1
   return s
 
-def transformGraph(nodes, inG, g):
+def transformGraph(nodes, ing, g):
   idxInG = {} # instead of storing node number, store index of node -> index of in-nodexs
   idxG = {}
   idx = dict(zip(nodes, range(len(nodes))))
-  for node, inNodes in inG.iteritems():
+  for node, inNodes in ing.iteritems():
     idxInG[idx[node]] = set([idx[inNode] for inNode in inNodes])
   for node, outDegree in g.iteritems():
     idxG[idx[node]] = outDegree #set([idx[outNode] for outNode in outNodes])
   return idxInG, idxG
 
-def transformToArray(nodes, inG, g):
+def transformToArray(nodes, ing, g):
   inA = array('l')
   a = array('l')
-  idx = dict(zip(nodes, range(len(nodes))))
-  idxInG, idxG = transformGraph(nodes, inG, g)
+  idxInG, idxG = transformGraph(nodes, ing, g)
+  ing_pos = array('l') # array contains the starting index in inA for each entry in ing
   i = 0
+  ing_cur = 0
   while i < len(nodes): #[<index>, <size>, <out node list>]
     if i in idxG:
-      a.append(i)
-      outNodes = idxG[i]
-      a.append(len(outNodes))
-      a.extend(outNodes)
+      a.append(idxG[i])
+    else:
+      a.append(0)
     if i in idxInG:
+      ing_pos.append(ing_cur)
       inA.append(i)
       inNodes = idxInG[i]
       inA.append(len(inNodes))
       inA.extend(inNodes)
+      ing_cur += len(inNodes) + 2
     i += 1
   
-  return inA, a
+  return Array('l',inA, lock=False), Array('l',a, lock=False), Array('l', ing_pos, lock=False)
 
-def fillVector(r, num, lower=0, higher=0):
+def fill_vector(r, num, lower=0, higher=0):
   if lower > higher:
     lower , higher = higher, lower
   i = lower
@@ -86,7 +88,7 @@ def log(msg):
   if _debug:
    print >>sys.stderr, msg
 
-def iterate(tid, r, r_, g, inG, idx, shared):
+def iterate(tid, r, r_, g, ing, idx, shared):
   t = 1
   e = shared['e']
   diff = e + 1
@@ -94,33 +96,36 @@ def iterate(tid, r, r_, g, inG, idx, shared):
   size = shared['size']
   lower = shared['lower'][tid]
   higher = shared['higher'][tid]
+  ing_low = shared['ing_low'][tid]
+  ing_high = shared['ing_high'][tid]
   beta = shared['beta']
-  log('T{0}: lower={1}, higher={2}'.format(tid, lower, higher))
+  log('T{0}: lower={1}, higher={2}; ing_low={3}, ing_high={4}'.format(tid, lower, higher, ing_low, ing_high))
 
   while diff > e and (n == -1 or t < n):
     sum_r = 0.0
     # fill in 0s
-    fillVector(r_, 0.0, lower, higher)
-    j = lower
-    while j < higher:
-      if j not in inG:
-	r_[j] = 0.0
-	j += 1
-	continue
+    fill_vector(r_, 0.0, lower, higher)
+    cursor = ing_low
+    while cursor < ing_high:
+      j = ing[cursor]
+      cursor += 1
+      row_size = ing[cursor]
+      cursor += 1
+      row_end = cursor + row_size
       rj = 0.0
-      iSet = inG[j]
-      for i in iSet:
+      while cursor < row_end :
+	i = ing[cursor]
 	rj += r[i] / g[i]
-      r_[j] = rj # multiply by beta when fixing leak
-      sum_r += rj # multiply by beta later
-      j += 1
+	cursor += 1
+      r_[j] += rj
+      sum_r += rj
     
     sum_r *= beta
-    shared['sum_r'][tid][0].send(sum_r)
+    shared['sum_r'][tid] = sum_r
     #shared['sum_r'][tid] = sum_r
     shared['children'][tid].set() # shared['childEvents'] is a list of threading.Event objects
     waitForMain(shared)
-    sum_r = shared['sum_r'][tid][0].recv()
+    sum_r = shared['sum_r'][tid]
     leaked = 1 - sum_r
     leakedShare = leaked / size if leaked > 0 else 0.0
     _i = lower
@@ -130,16 +135,16 @@ def iterate(tid, r, r_, g, inG, idx, shared):
       _i +=1
 
     diff = diffVector(r, r_, lower, higher)
-    shared['diff'][tid][0].send(diff)
+    shared['diff'][tid] = diff
     shared['children'][tid].set()
     waitForMain(shared)
 
-    diff = shared['diff'][tid][0].recv()
+    diff = shared['diff'][tid]
     r, r_ = r_, r
     t += 1
 
 
-def pageRank(nodes, g, inG, beta=0.8, e=1e-8, n=1e2, threads=4):
+def pageRank(nodes, g, ing, ing_pos, beta=0.8, e=1e-8, n=1e2, threads=4):
   size = len(nodes)
   r = Array(c_double,[1.0/size] * size, lock=False)
   r_ = Array(c_double,r, lock=False)
@@ -151,21 +156,25 @@ def pageRank(nodes, g, inG, beta=0.8, e=1e-8, n=1e2, threads=4):
       'size': size,
       'lower': [0] * threads, 
       'higher': [0] * threads, 
+      'ing_low': [0] * threads,
+      'ing_high': [0] * threads,
       'beta': beta,
-      'sum_r': [None] * threads, 
+      'sum_r': Array(c_double, [0.0] * threads, lock=False), 
       'children': [None] * threads, 
       'main': Event(),
-      'diff': [None] * threads,
+      'diff': Array(c_double, [0.0] * threads, lock=False) ,
       'threads': threads
       }
 
   for tid in range(threads):
     shared['lower'][tid] = tid * size / threads
     shared['higher'][tid] = (tid + 1) * size / threads
+    shared['ing_low'][tid] = ing_pos[tid * len(ing_pos) / threads]
+    shared['ing_high'][tid] = ing_pos[(tid + 1 ) * len(ing_pos) / threads - 1] + 1
     shared['children'][tid] = Event()
-    shared['sum_r'][tid] = Pipe()
-    shared['diff'][tid] = Pipe()
-    threadPool.append(Process(target=iterate, args=(tid, r, r_, g, inG, idx, shared)))
+#    shared['sum_r'][tid] = Pipe()
+#    shared['diff'][tid] = Pipe()
+    threadPool.append(Process(target=iterate, args=(tid, r, r_, g, ing, idx, shared)))
     threadPool[-1].start()
   diff = 1 + e
   t = 1
@@ -173,13 +182,17 @@ def pageRank(nodes, g, inG, beta=0.8, e=1e-8, n=1e2, threads=4):
     waitForChildren(shared)
 
     # now to compute the whole sum_r
-    sum_r = sum([tup[1].recv() for tup in shared['sum_r']])
-    temp = [tup[1].send(sum_r) for tup in shared['sum_r']]
+#    sum_r = sum([tup[1].recv() for tup in shared['sum_r']])
+    sum_r = sum(shared['sum_r'])
+    fill_vector(shared['sum_r'], sum_r)
+#    temp = [tup[1].send(sum_r) for tup in shared['sum_r']]
     shared['main'].set()
 
     waitForChildren(shared) 
-    diff = sum([tup[1].recv() for tup in shared['diff']])
-    temp = [tup[1].send(diff) for tup in shared['diff']]
+#    diff = sum([tup[1].recv() for tup in shared['diff']])
+    diff = sum(shared['diff'])
+    fill_vector(shared['diff'], diff)
+#    temp = [tup[1].send(diff) for tup in shared['diff']]
 
     shared['main'].set()
     t += 1
@@ -201,27 +214,29 @@ if __name__ == '__main__':
   import fileinput
   import time
   import argparse
-  
+  import os
+
   parser = argparse.ArgumentParser()
   parser.add_argument('-v', dest='debug', action='store_true')
   parser.add_argument('-p', dest='p', type=int, default=1)
   args = parser.parse_args()
  
+  print >> sys.stderr, 'PID: {0}'.format(os.getpid())
   _debug = args.debug
   timer = 0 - time.time()
   lines = fileinput.input()
-  g, inG, nodes = readGraph(sys.stdin)
+  g, ing, nodes = readGraph(sys.stdin)
   timer += time.time()
   print >> sys.stderr, 'Read graph: {0:.1f} seconds.'.format(timer)
 
   timer = 0 - time.time()
-  inG, g = transformGraph(nodes, inG, g)
+  ing, g, ing_pos = transformToArray(nodes, ing, g)
   timer += time.time()
   print >> sys.stderr, 'Preprocess graph: {0:.1f} seconds'.format(timer)
 
   log('Processes: {0}'.format(args.p))
   timer = 0 - time.time() 
-  r, t = pageRank(nodes, g, inG, threads=args.p)
+  r, t = pageRank(nodes, g, ing, ing_pos, threads=args.p)
   timer += time.time()
   print >> sys.stderr, 'Page Rank: {0:.1f} seconds.'.format(timer)
   for idx, i in enumerate(r):
